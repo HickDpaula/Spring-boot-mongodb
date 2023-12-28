bb7dd696863dc9e637d17cba59c58e07c450d25c
Henrique Santos <hickdpaula@gmail.com>
2023-12-28 15:26:39 -0300

    insercao de usuario com POST

--- src/main/java/com/example/workshopMongodb/demo/resources/UserResource.java
+++ src/main/java/com/example/workshopMongodb/demo/resources/UserResource.java
@@ -6,11 +6,10 @@ import com.example.workshopMongodb.demo.entities.User;
 import com.example.workshopMongodb.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,7 +21,8 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity <List<UserDTO>> findAll(){
-     List<User> list = userService.findAll();List<UserDTO> listDto = list.stream().map(user -> new UserDTO (user)).collect(Collectors.toList());
+     List<User> list = userService.findAll();
+     List<UserDTO> listDto = list.stream().map(user -> new UserDTO (user)).collect(Collectors.toList());
       return ResponseEntity.ok().body(listDto);
 
     }
@@ -30,5 +30,15 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
          User obj = userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> Insert(@RequestBody UserDTO objDTO){
+        User obj = userService.fromDTO(objDTO);
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
+
     }
 }
--- src/main/java/com/example/workshopMongodb/demo/services/UserService.java
+++ src/main/java/com/example/workshopMongodb/demo/services/UserService.java
@@ -1,5 +1,6 @@
 package com.example.workshopMongodb.demo.services;
 
+import com.example.workshopMongodb.demo.dto.UserDTO;
 import com.example.workshopMongodb.demo.entities.User;
 import com.example.workshopMongodb.demo.repository.UserRepository;
 import com.example.workshopMongodb.demo.services.exception.ObjectNotFoundException;
@@ -23,5 +24,14 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
